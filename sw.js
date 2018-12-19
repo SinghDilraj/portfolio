9f103ba679e9e51afb693639bb68f1d16af1f42b
Dilraj Singh <dilrajkehra@gmail.com>
2018-12-19 14:05:26 -0600

    old cache delete function added to sw.js

--- sw.js
+++ sw.js
@@ -27,6 +27,21 @@ self.addEventListener('install', event => {
   )
 })
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.filter(cacheName => {
+            return cacheName.startsWith('my-cache') && cacheName !== staticCache
+          }).map(cacheName => {
+            return caches.delete(cacheName)
+          })
+        )
+      })
+  )
+})
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
