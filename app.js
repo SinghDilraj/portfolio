fef5ec41d84337f4fe303bd4e9b569e9f006e972
Dilraj Singh <dilrajkehra@gmail.com>
2018-12-19 13:27:38 -0600

    service worker registered in app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+      .then(reg => {
+        console.log('service worker is registered', reg)
+      })
+      .catch(err => {
+        console.log('service worker is not registered', err)
+      })
+  })
+}
